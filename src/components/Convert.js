976f62d78dee62a715ae6b3eecbfc53320353016
Raymund Caringal <rcrock1978@gmail.com>
2021-01-26 15:44:16 +0800

    axios

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,19 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
   useEffect(() => {
-    console.log("New language or text");
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: "AIzaSyDthwLOhhTgIVIIFyp3Ff9w23wme80Gn8U"
+        }
+      }
+    );
   }, [language, text]);
 
   return <div />;
